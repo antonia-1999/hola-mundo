46e6eef2a73dbcc3469e60e0622dd7224bd6b57b
antonia-1999 <1999antonia.rodriguez@gmail.com>
2024-07-06 06:26:55 +0000

    bifurcacion

--- holamundo.cpp
+++ holamundo.cpp
@@ -3,8 +3,16 @@
 
 int main ()
 {
-    std::string msg{"hola mundo y alrededores"};
-    msg = msg+" que tengan un buen dia";
+    std::string msg{"hola mundo y alrededores "};
+    int hora_actual {19};
+    msg += "que tengan";
+
+    if (hora_actual>6 && hora_actual<19 ) {
+        msg += " buenos dias";
+    }
+    else {
+        msg += " buenas noches";
+    }
     std::puts (msg.c_str());
     return 0; 
 }
